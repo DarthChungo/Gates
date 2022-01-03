94edb7fab21a4dd544da62192241afd35606f184
DarthChungo <antonio@adaro.eu>
2022-01-03 16:36:39 +0100

    Added GPL3 to project

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Gates
 =====
 
-Todo
+Un simple editor de circuitos lógicos programado en C++.
+
+Licencia
+========
+
+Este software está distribuido bajo la licencia GNU GPL versión 3.
+
--- src/Gates/GatesApplication.cpp
+++ src/Gates/GatesApplication.cpp
@@ -1,3 +1,21 @@
+/*
+Gates, a simple logic circuit simulator written in C++
+Copyright (C) 2022 DarthChungo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "Engine/Renderer.hpp"
 #include "Gates/GatesApplication.hpp"
 #include "Gates/LogicGates.hpp"
@@ -146,6 +164,16 @@ namespace Gates {
       if (ImGui::BeginMenu("Ver")) {
         ImGui::Checkbox("Estadísticas", &show_statistics);
         ImGui::Checkbox("Controles", &show_controls);
+        ImGui::EndMenu();
+      }
+
+      if (ImGui::BeginMenu("Acerca de")) {
+        if (ImGui::MenuItem("Licencia")) {
+        }
+
+        if (ImGui::MenuItem("Versión")) {
+        }
+
         ImGui::EndMenu();
       }
 
